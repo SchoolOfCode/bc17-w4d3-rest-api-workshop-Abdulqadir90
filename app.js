96f9b2ab687a8f81926b39e00dc9dad0b059d4a9
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-07-27 17:34:15 +0100

    task 2 done

--- app.js
+++ app.js
@@ -15,21 +15,7 @@ app.use(express.json()); // enables JSON for incoming requests
 const PORT = 3000;
 
 
-  app.get('/astronauts', async (req, res) => { // sets up the route handler for GET requests with path of /astronauts
-    try {
-      const astronauts = await getAstronauts(); // getastronauts is called to fetch the data
-      res.json({
-        success: true, 
-        payload: astronauts
-      });
-    } catch (error) {
-      res.status(500).json({ // if an error occurs, a 500 status is returned with error message
-        success: false,
-        payload: "Cant get astronauts data"
-      });
-      
-    }
-  });
+  
 
 
 app.listen(PORT, ()=> {
@@ -53,10 +39,43 @@ res.json({
 /* Write a request handler to return the correct response when a `GET` request is received to `/astronauts`. Choose the appropriate 
 function from the imported functions at the top of the `app.js` to get your data. */
 
+app.get('/astronauts', async (req, res) => { // sets up the route handler for GET requests with path of /astronauts
+  try {
+    const astronauts = await getAstronauts(); // getastronauts is called to fetch the data
+    res.json({
+      success: true, 
+      payload: astronauts
+    });
+  } catch (error) {
+    res.status(500).json({ // if an error occurs, a 500 status is returned with error message
+      success: false,
+      payload: "Cant get astronauts data"
+    });
+    
+  }
+});
+
 // Task 2
 
 /* Write a request handler to return the correct response and perform the correct action when a `POST` request is received to 
 `/astronauts`. Choose the appropriate function from the imported functions at the top of the `app.js` to perform the action. */
+
+app.post('/astronauts', async (req, res) => { // sets up the route handler for GET requests with path of /astronauts
+  try {
+    const newAstronauts = await createAstronaut(req.body); // createastronaut is called to fetch the data sent in the body of a http request
+    res.json({
+      success: true, 
+      payload: newAstronauts
+    });
+  } catch (error) {
+    res.status(500).json({ // if an error occurs, a 500 status is returned with error message
+      success: false,
+      payload: "Cant get astronauts data"
+    });
+    
+  }
+});
+
 
 // Task 3
 
